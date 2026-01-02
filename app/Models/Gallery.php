b834ac4db47204674e992c8e28e003501270a6f5
vincent <daylightzone91@gmail.com>
2026-01-02 11:02:35 +0700

    update galeri CRUD

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Gallery extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'image',
+        'description',
+        'is_active',
+        'category_id',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
